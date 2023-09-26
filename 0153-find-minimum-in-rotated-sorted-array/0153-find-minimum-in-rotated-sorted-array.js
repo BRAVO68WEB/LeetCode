4144bbc48abe3e2a4e6dcdf9a69956e7fa67b024
Jyotirmoy Bandyopadhayaya <jbandyopadhayaya@gmail.com>
2023-09-27 03:16:31 +0530

    Time: 48 ms (75.37%), Space: 42.1 MB (40.83%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -3,19 +3,5 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    let left = 0;
-    let right = nums.length-1;
-    let mid;
-    
-    while(left < right) {
-        mid = Math.floor((left + right) / 2);
-        
-        if(nums[mid] > nums[right]) {
-            left = mid + 1;
-        } else {
-            right = mid;
-        }
-    }
-    
-    return nums[left];
+    return Math.min(...nums);
 };
