d6b308c3247cba15a18ad67bf5e1f9e2f48afaef
Jyotirmoy Bandyopadhayaya <jbandyopadhayaya@gmail.com>
2023-09-27 06:27:37 +0530

    Time: 41 ms (98.93%), Space: 42.8 MB (60.09%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let map = new Map()
+    for(let i=0; i<numbers.length; i++){
+        let diff = target - numbers[i]
+        if(map.has(diff)){
+            return [map.get(diff)+1, i+1]
+        } else {
+            map.set(numbers[i], i)
+        }
+    }
+};
