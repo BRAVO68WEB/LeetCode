71eb54131ba0c51a65d75582cbd0a16ff57db15c
Jyotirmoy Bandyopadhayaya <jbandyopadhayaya@gmail.com>
2024-11-02 23:50:41 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (33.36%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+        int count = 0;
+        while (n) {
+            count += n & 1;
+            n >>= 1;
+        }
+        return count;
+    }
+};
