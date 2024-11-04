1e608b358dd4f4ab027acc8a231ef15ec9a648d4
Jyotirmoy Bandyopadhayaya <jbandyopadhayaya@gmail.com>
2024-11-04 18:47:23 +0530

    Time: 0 ms (100%), Space: 22.3 MB (83.07%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -5,15 +5,15 @@ using namespace std;
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        vector<unordered_set<char>> row(9), col(9), box(9);
+        array<bitset<9>, 9> row, col, box;
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 char c = board[i][j];
                 if (c == '.') continue;
-                if (row[i].count(c) || col[j].count(c) || box[i / 3 * 3 + j / 3].count(c)) return false;
-                row[i].insert(c);
-                col[j].insert(c);
-                box[i / 3 * 3 + j / 3].insert(c);
+                int num = c - '1';
+                int boxIndex = (i / 3) * 3 + (j / 3);
+                if (row[i][num] || col[j][num] || box[boxIndex][num]) return false;
+                row[i][num] = col[j][num] = box[boxIndex][num] = 1;
             }
         }
         return true;
